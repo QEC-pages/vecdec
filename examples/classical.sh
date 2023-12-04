3ee19d51827af2a64f8f063e609c75e366709240
Leonid Pryadko <leonid.pryadko@gmail.com>
2023-12-03 19:22:04 -0800

    script updated

--- examples/classical.sh
+++ examples/classical.sh
@@ -6,7 +6,7 @@ debug=0;
 for ((ii=0; ii <= 12; ii++)); do
     pp=`awk "BEGIN { print ${p0}*exp(-${ii}*log(2)*0.25) }"`
     echo ii $pp
-    $prog debug=$debug finH=$code nvec=$((1000*1000)) nfail=1000 useP=$pp > tmp.tmp
+    $prog debug=$debug finH=$code nvec=$((1000*1000)) nfail=1000 useP=$pp qllr2=0 > tmp.tmp
     cat tmp.tmp
     echo $pp `cat tmp.tmp` >> $outfile
     debug=0;
