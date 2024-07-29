456f6a06aa074a693f0b187317f12343ad9f91d3
Leonid Pryadko <leonid.pryadko@gmail.com>
2024-07-29 10:25:23 -0700

    intermediate save

--- src/vecdec.h
+++ src/vecdec.h
@@ -84,10 +84,14 @@ extern "C"{
     char *perr; /** `output file` name for `predicted` error vectors */
     char *finU; /** `input file` name for `U` (list of likele error vectors for decoding) */
     char *outU; /** `output file` name for `U` (list of non-trivial error vectors for decoding) */
+    long long int numU; /** number of syndrome vectors in `U` hash */
+    long long int numE; /** total number of error vectors -> syndrome vectors in `U` hash */
     long long int maxU; /** max number of syndrome vectors in `U` hash */
     qllr_t uE; /** max energy of an error vector in `U` hash*/
     double uEdbl; /** max energy of an error vector in `U` hash*/
     int uW; /** max weight of an error vector in `U` hash*/
+    three_vec_t *hashU; /** `U` hash location */
+    int *permHe; /** permutation vector for syndrome bits when hashU is used */
     int classical; /** `1` if this is a classical code? */
     int internal; /** `1` to generate obs/det internally, `2` to generate from `err` file */
     long long int seed;  /** rng `seed`, set<=0 for automatic */
