1f10e42d8cb7123f2cb50b8dc51d8c42799a11ce
Leonid Pryadko <leonid.pryadko@gmail.com>
2024-09-12 23:16:40 -0700

    documentation update

---
+++ examples/ex_01.sh
@@ -0,0 +1,7 @@
+
+stim=../../Stim/out/stim # location of `stim` command-line binary
+vecdec=../src/vecdec     # location of `vecdec`
+$stim sample_dem --shots 5000 --in ./examples/surf_d3.dem \
+	--out tmp_det.01 --out_format 01 \
+	--obs_out tmp_obs.01 --obs_out_format 01
+$vecdec mode=0 fdet=tmp_det.01 fobs=tmp_obs.01 fdem= ./examples/surf_d3.dem nvec=2500 steps=1000 ntot=5000 
