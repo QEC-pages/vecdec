76be35418afde50a940fc854af7872e4627eaf82
Leonid Pryadko <leonid.pryadko@gmail.com>
2024-12-01 14:56:07 -0800

    check linear independence of rows of L from H

--- src/util_m4ri.h
+++ src/util_m4ri.h
@@ -524,6 +524,9 @@ void make_err(mzd_t *row, double p);
 /** @brief calculate the rank of the csr matrix `M` */
   int rank_csr(const csr_t * const M);  
 
+/** @brief calculate the joint rank of two matrices stacked on top of each other */
+  int rank_stacked(const csr_t * const H, const csr_t * const L);
+  
 #if defined(__cplusplus) && !defined (_MSC_VER)
 }
 #endif
